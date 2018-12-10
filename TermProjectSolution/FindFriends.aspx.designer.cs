5aa7979a7fa52c9682815aff956b72c1d7a40e38
Spencer <tuf89789@temple.edu>
2018-12-09 19:37:29 -0500

    fixed enter issue on find friends

--- TermProjectSolution/FindFriends.aspx.designer.cs
+++ TermProjectSolution/FindFriends.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace TermProjectSolution {
         protected global::System.Web.UI.WebControls.Button btnLogOut;
         
         /// <summary>
+        /// p control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel p;
+        
+        /// <summary>
         /// lblSearch control.
         /// </summary>
         /// <remarks>
